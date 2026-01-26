f615113e7dbfd19a6078ae2a31304f4e4aad04ad
Leonardo Silveira <sombriks@gmail.com>
2026-01-26 09:34:09 -0300

    feat(#5): wiring testcases for the new user collection endpoint

--- src/test/java/sample/pokedex/restapi/usercollection/UserCollectionTest.java
+++ src/test/java/sample/pokedex/restapi/usercollection/UserCollectionTest.java
@@ -1,4 +1,4 @@
-package sample.pokedex.restapi.pokemon;
+package sample.pokedex.restapi.usercollection;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -7,23 +7,14 @@ import org.springframework.boot.resttestclient.TestRestTemplate;
 import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureTestRestTemplate;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import sample.pokedex.restapi.TestcontainersConfiguration;
-import sample.pokedex.restapi.pokemon.dto.PokemonDto;
 import sample.pokedex.restapi.security.dto.UserPass;
-
-import java.util.HashMap;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.notNullValue;
 
 @AutoConfigureTestRestTemplate
 @Import({TestcontainersConfiguration.class})
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class PokeCtlTest {
+public class UserCollectionTest {
 
     @Autowired
     TestRestTemplate restTemplate;
@@ -40,18 +31,22 @@ public class PokeCtlTest {
 
     @Test
     void shouldList() {
-        var headerEntity = new HttpEntity<Void>(headers);
-        var result = restTemplate.exchange("/v1/pokemons", HttpMethod.GET, headerEntity, HashMap.class);
-        assertThat(result.getStatusCode().is2xxSuccessful(), is(true));
-        assertThat(result.getBody().get("content"), notNullValue());
     }
 
     @Test
     void shouldFind() {
-        var headerEntity = new HttpEntity<Void>(headers);
-        var result = restTemplate.exchange("/v1/pokemons/4", HttpMethod.GET, headerEntity, PokemonDto.class);
-        assertThat(result.getStatusCode().is2xxSuccessful(), is(true));
-        assertThat(result.hasBody(), is(true));
-        assertThat(result.getBody().name(), is("charmander"));
     }
+
+    @Test
+    void shouldInsert() {
+    }
+
+    @Test
+    void shouldUpdate() {
+    }
+
+    @Test
+    void shouldDelete() {
+    }
+
 }
